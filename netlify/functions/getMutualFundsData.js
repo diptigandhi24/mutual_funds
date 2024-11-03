4ac90c937b810987011f1dab24dd29153fed3579
diptigandhi24 <gandhidipti.s@gmail.com>
2024-11-03 13:45:58 +0530

    trying to find the function file

---
+++ netlify/functions/getMutualFundsData.js
@@ -0,0 +1,10 @@
+// export default function getMutualFundsData() {
+//   return "You are receiving data from Netlify";
+// }
+
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: "hellllllloooooo WORLĎ",
+  };
+};
